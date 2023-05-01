0ad1cf3a67ddf99b27edad87b1eb84c842e07883
Yevhen Hryshchenko <eugene.grishchenko@live.com>
2023-05-01 21:15:43 +0300

    Added Header with summary labels and basic styling

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employees book</h1>
+            <h2>Total employees count: </h2>
+            <h2>People with bonus count: </h2>
+        </div>
+    );
+};
+
+export default AppInfo;
