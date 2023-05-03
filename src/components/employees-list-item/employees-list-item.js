905ce131fdfbd14f97798dfb957f35c0d8f04b61
Yevhen Hryshchenko <eugene.grishchenko@live.com>
2023-05-03 17:34:42 +0300

    Rewrited employee-list-item component into class component  in order to learn component state

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,8 +1,15 @@
 import './employees-list-item.css'
+import {Component} from 'react';
 
-const EmployeeListItem = ({name, salary, increase}) => {
-    return (
-        <li className={"list-group-item d-flex justify-content-between" + (increase? ' increase' : '')}>
+class EmployeeListItem extends Component () {
+    constructor (props) {
+        super(props);
+        //no actions above eslint says that this contructor is useless but fo educational reason i'll left it
+    };
+    render (){
+        const {name, salary, increase} = this.props;
+        return(
+            <li className={"list-group-item d-flex justify-content-between" + (increase? ' increase' : '')}>
             <span className="list-group-item-label">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
@@ -18,7 +25,8 @@ const EmployeeListItem = ({name, salary, increase}) => {
                 <i className="fa-solid fa-star"></i>
             </div>
         </li>
-    );
+        );
+    };
 };
 
 export default EmployeeListItem;
